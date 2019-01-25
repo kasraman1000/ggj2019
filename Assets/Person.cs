a1957ca3a143cacee81e9e92cd61c39cf7b6185b
kasraman1000 <minutegreywing@gmail.com>
2019-01-25 21:32:06 +0100

    Pickup and put down stuffs

--- Assets/Person.cs
+++ Assets/Person.cs
@@ -1,9 +1,31 @@
-﻿using UnityEngine;
+﻿using UnityEditor;
+using UnityEngine;
 
 public class Person : MonoBehaviour {
 
+    [Header("Configs")]
     [SerializeField]
     private float Speed = 1.0f;
+
+    [SerializeField]
+    private float PickupDistance = 1.0f;
+
+
+    [Header("Gameobject references")] 
+    
+    [SerializeField]
+    Transform stuffHoldingAnchor;
+    
+    Vector2 facingDir;
+
+    Stuff holding;
+
+    void OnValidate() {
+        if (stuffHoldingAnchor == null) {
+            Debug.LogError("no stuffholdingachor assigned", this);
+        }
+    }
+
 
     private void Start() {
 
@@ -29,5 +51,52 @@ public class Person : MonoBehaviour {
         }
 
         transform.position += (Vector3)moveDir * Speed * Time.deltaTime;
+
+        if (moveDir != Vector2.zero) {
+            facingDir = moveDir;            
+        }
+
+        Debug.DrawLine(transform.position, transform.position + (Vector3) facingDir * PickupDistance);
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (holding != null) {
+                // put sutff down
+                putDownStuff();
+                
+            } else {
+                // pick stuff up
+                
+                // step 1 find stuff :D
+
+                int layerMask = LayerMask.GetMask("Stuff");
+                
+                var hit = Physics2D.Raycast(transform.position, facingDir, PickupDistance,
+                    layerMask); // This is inefficient layermask stuff lul
+
+                Debug.Log(hit.collider, hit.collider.gameObject);
+                
+                if (hit.collider != null) {
+                    var stuff = hit.collider.gameObject.GetComponent<Stuff>();
+                    
+                    Debug.Assert(stuff != null);
+
+                    pickUpStuff(stuff);
+                }
+            }
+        }
+    }
+
+    private void pickUpStuff(Stuff stuff) {
+        holding = stuff;
+
+        stuff.transform.SetParent(stuffHoldingAnchor);
+        stuff.transform.localPosition = Vector2.zero;
+    }
+
+    private void putDownStuff() {
+        holding.transform.SetParent(null);
+        holding.transform.position = transform.position + (Vector3) facingDir * PickupDistance;
+
+        holding = null;
     }
 }
