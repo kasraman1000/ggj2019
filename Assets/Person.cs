d3794694e34aab10b4fbe5b1b69320bf98b4c9cd
kasraman1000 <minutegreywing@gmail.com>
2019-01-25 20:18:54 +0100

    first cowmit

---
+++ Assets/Person.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour {
+
+    float speed = 1.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update() {
+        var moveDir = Vector2.zero;
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow)) {
+            moveDir = moveDir + Vector2.up;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)) {
+            moveDir = moveDir + Vector2.down;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            moveDir = moveDir + Vector2.left;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            moveDir = moveDir + Vector2.right;
+        }
+
+        transform.position += (Vector3) moveDir * speed;
+    }
+}
