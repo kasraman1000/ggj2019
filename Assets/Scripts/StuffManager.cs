f9d491e57a5d0722744c9363f9b04b8c7b99392b
kasraman1000 <minutegreywing@gmail.com>
2019-01-26 21:50:08 +0100

    scripted regeneration of stufflists

--- Assets/Scripts/StuffManager.cs
+++ Assets/Scripts/StuffManager.cs
@@ -1,17 +1,55 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+[CustomEditor(typeof(StuffManager))]
+public class StuffManagerEditor : Editor {
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+        GUILayout.Space(20);
+
+        if (GUILayout.Button("Regenerate List")) {
+            regenerateList();
+        }
+    }
+
+    void regenerateList() {
+        var assets = AssetDatabase.FindAssets("t:StuffTemplate", new[]{"Assets/Stuff"});
+
+        var templates = new List<StuffTemplate>();
+        
+        foreach (var guid in assets) {
+            var assetPath = AssetDatabase.GUIDToAssetPath(guid);
+
+            StuffTemplate loadedAsset = AssetDatabase.LoadAssetAtPath<StuffTemplate>(assetPath);
+
+            if (loadedAsset == null) {
+                continue;
+            }
+
+            templates.Add(loadedAsset);
+            
+            
+        }
+
+        ((StuffManager) target).stuffList = templates.ToArray();
+    }
+}
+    
+#endif
+
 
 [CreateAssetMenu]
 public class StuffManager : ScriptableObject {
-    [SerializeField]
-    StuffTemplate[] _stuffList = null;
+    public StuffTemplate[] stuffList = null;
 
     [SerializeField]
     Stuff stuffPrefab = null;
 
-    public StuffTemplate[] stuffList => _stuffList;
-
     public Stuff makeRandomStuff() {
-        var stuffToMake = _stuffList[Random.Range(0, _stuffList.Length-1)];
+        var stuffToMake = stuffList[Random.Range(0, stuffList.Length-1)];
 
         return makeStuff(stuffToMake);
     }
