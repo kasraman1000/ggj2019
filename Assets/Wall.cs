cfe2ae2cb2074fa4213581bf095c43767cec9420
Oliver Phillip Roer <phazyck@gmail.com>
2019-01-26 17:47:35 +0100

    add texs, build a wall

---
+++ Assets/Wall.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Grow(Vector2 direction) {
+
+    }
+}
